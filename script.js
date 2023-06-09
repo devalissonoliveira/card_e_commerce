0fbc00b88ce3e122d312ef10a64474980a3221d8
devalissonoliveira <devalissonoliveira@gmail.com>
2023-06-08 23:06:42 -0300

    Adicionado o preço em uma interação de mouseover no botão de adicionar ao carrinho

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-let btnAddCancel = document.querySelector(".btn-add");
-let iconAddCancel = document.querySelector(".material-symbols-outlined");
+const subTituloTenisContainer = document.querySelector(".sub-titulo-tenis-container");
+const subTituloTenisContainerOriginal = subTituloTenisContainer.innerHTML;
 
+const subTituloTenis = document.querySelector(".sub-titulo-tenis");
+
+const btnAddCancel = document.querySelector(".btn-add");
+const iconAddCancel = document.querySelector(".material-symbols-outlined");
+
+function precoOver(){
+    subTituloTenisContainer.innerHTML = `<p class="preco">R$ 279,00</p>`;
+}
+
+function DevolverSubTituloTenis(){
+    subTituloTenisContainer.innerHTML = `${subTituloTenisContainerOriginal}`;
+}
+
+iconAddCancel.addEventListener("mouseover", precoOver);
+iconAddCancel.addEventListener("mouseout", DevolverSubTituloTenis);
 
 function addCancel(){
     if (iconAddCancel.classList.contains("cancel")){
-    iconAddCancel.classList.remove("cancel")
-    iconAddCancel.innerText = "add_circle";
+        iconAddCancel.classList.remove("cancel");
+        iconAddCancel.innerText = "add_circle";
     } else{
-        iconAddCancel.classList.add("cancel")
+        iconAddCancel.classList.add("cancel");
         iconAddCancel.innerText = "cancel";
     }
 }
-
 
 btnAddCancel.addEventListener("click", addCancel);
